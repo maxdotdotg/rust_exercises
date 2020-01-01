433464166507a19285034c11d8785e23e2bf824b
Max G <maxdotdotg@gmail.com>
2020-01-01 15:24:44 -0500

    example of using tuples and pretty-print for debugging

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,15 @@ fn main() {
     let heart_eyed_cat = '😻';
     println!("{}, {}, {}",c, z, heart_eyed_cat);
 
+    // tuples are fixed length, can't be modified
+    // type annotation below for reference, it's not required
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x,y,z) = tup;
 
+    // this doesn't work with the default formater
+    // println!("{}", tup);
+    println!("pretty print of tup: {:#?}", tup); // this is pretty-print for debugging
+    println!("tup is {:?}", tup); // this is print for debugging
+    println!("the vlaue of y is {}", y);
 }
 
